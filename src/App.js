038f32b68fbb4f1da66446fd42787736acf18119
Emyli Poltorak <emylipoltorak@Emylis-MacBook-Pro-1126.local>
2018-01-08 14:49:30 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SearchBar from './components/search-bar';
+
+const API_KEY="AIzaSyAY8BMNLxjCKM7LdghGoaXCQqR1L2Nxbx4";
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <SearchBar />
+            </div>
+
+        );
+    }
+}
+
+export default App
+
